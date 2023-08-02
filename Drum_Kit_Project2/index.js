bd19dd0e331c9852674a8a357ad79af6be9a69e9
Saam26 <saamyukkth@gmail.com>
2023-08-02 17:11:52 +0530

    modified with switch statements

--- Drum_Kit_Project2/index.js
+++ Drum_Kit_Project2/index.js
@@ -1,55 +1,35 @@
-
-var i;
-// for (i=0;i<6;i++)
-// {
-//     document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
-// }
-document.querySelector(".w").addEventListener("click",handleClickw);
-document.querySelector(".a").addEventListener("click",handleClicka);
-document.querySelector(".b").addEventListener("click",handleClickb);
-document.querySelector(".c").addEventListener("click",handleClickc);
-document.querySelector(".d").addEventListener("click",handleClickd);
-document.querySelector(".e").addEventListener("click",handleClicke);
-
-function handleClickw()
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
+for (var i=0;i<numberOfDrumButtons;i++)
 {
-    var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/crash.mp3");
-    this.style.color="blue";
-    audio.play();
-};
-
-function handleClicka()
-{
-    var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/kick-bass.mp3");
-    this.style.color="blue";
-    audio.play();
-};
-
-function handleClickb()
-{
-    var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/snare.mp3");
-    this.style.color="blue";
-    audio.play();
-};
-
-function handleClickc()
-{
-    var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-1.mp3");
-    this.style.color="blue";
-    audio.play();
-};
-
-function handleClickd()
-{
-    var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-2.mp3");
-    this.style.color="blue";
-    audio.play();
-};
-
-function handleClicke()
-{
-    var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-3.mp3");
-    this.style.color="blue";
-    audio.play();
-};
-
+    document.querySelectorAll(".drum")[i].addEventListener("click",function()
+    {
+        var buttonInnerHTML = this.innerHTML;
+        switch(buttonInnerHTML)
+        {
+            case "w":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "b":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/snare.mp3");
+                audio.play();
+                break;
+            case "c":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "e":
+                var audio = new Audio("/Users/samsidsuresh/Web development/Drum Kit Starting Files/sounds/tom-3.mp3");
+                audio.play();
+                break;
+        }
+    })
+}
