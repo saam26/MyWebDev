8fd177f7ac0983b987f2a4b5b520b8549b8f35eb
Saam26 <saamyukkth@gmail.com>
2023-08-12 17:44:53 +0530

    adding qr code project

---
+++ Qr_Code_generator_project3/index.js
@@ -0,0 +1,10 @@
+// import generateName from "sillyname";
+
+// var sillyname = generateName();
+
+// console.log(`My name is ${sillyname}.`);
+
+
+// import superheroes from "superheroes";
+// const name = superheroes.random();
+// console.log(`I am ${name}.`);
