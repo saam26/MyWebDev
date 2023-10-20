a3fbd87efcc0f333564adb55e411ca9fada84c5e
saam26 <saamyukkth@gmail.com>
2023-10-20 18:52:48 +0530

    Adding React project 1 folder

---
+++ keeper_app_project_1/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />,document.getElementById("root"));
+
+// const date = new Date();
+// const currentTime = date.getHours();
+
+// const customStyle = {
+//     background-image: linear-gradient(90deg, #FFF57B 0%, #FFE469 15%, #FECC51 35%, #FCB033 45%, #FA961B 55%, #FA8607 80%);
+// }
+
+// if (currentTime<12)
+// {
+
+// }
+
+//1. Create a new React app.
+//2. Create a App.jsx component.
+//3. Create a Header.jsx component that renders a <header> element
+//to show the Keeper App name in an <h1>.
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
